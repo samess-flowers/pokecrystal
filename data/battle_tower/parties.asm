ef27cf9fa6631da5e493e9c3659583492b92709d
Sam Ess <samess.flowers@gmail.com>
2024-04-28 08:15:50 -0400

    Add Fairy Type; Change Pink Bow to Silk Scarf; Make Polkadot Bow available and boost Fairy type

--- data/battle_tower/parties.asm
+++ data/battle_tower/parties.asm
@@ -1022,7 +1022,7 @@ BattleTowerMons:
 	db "PUKURIN@@@@"
 
 	db WIGGLYTUFF
-	db PINK_BOW
+	db SILK_SCARF
 	db PSYCHIC_M, SWAGGER, PSYCH_UP, HEADBUTT
 	dw 0 ; OT ID
 	dt 8000 ; Exp
@@ -3088,7 +3088,7 @@ BattleTowerMons:
 	db "HAPINASU@@@"
 
 	db SNORLAX
-	db PINK_BOW
+	db SILK_SCARF
 	db ROCK_SLIDE, SURF, BODY_SLAM, EARTHQUAKE
 	dw 0 ; OT ID
 	dt 216000 ; Exp
@@ -4891,7 +4891,7 @@ BattleTowerMons:
 	db "MIRUTANKU@@"
 
 	db TAUROS
-	db PINK_BOW
+	db SILK_SCARF
 	db THUNDERBOLT, HYPER_BEAM, ATTRACT, EARTHQUAKE
 	dw 0 ; OT ID
 	dt 729000 ; Exp

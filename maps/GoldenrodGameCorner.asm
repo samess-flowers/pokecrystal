2f8c497fb80f1b00ab3f1230c4d11048a4876806
Sam Ess <samess.flowers@gmail.com>
2024-04-28 17:57:09 -0400

    Remove the Elite Four check for the Move Tutor; Made him available  Wed/Sat/Sun

--- maps/GoldenrodGameCorner.asm
+++ maps/GoldenrodGameCorner.asm
@@ -39,13 +39,12 @@ GoldenrodGameCorner_MapScripts:
 	callback MAPCALLBACK_OBJECTS, GoldenrodGameCornerMoveTutorCallback
 
 GoldenrodGameCornerMoveTutorCallback:
-	checkevent EVENT_BEAT_ELITE_FOUR
-	iffalse .finish
 	checkitem COIN_CASE
 	iffalse .move_tutor_inside
 	readvar VAR_WEEKDAY
 	ifequal WEDNESDAY, .move_tutor_outside
 	ifequal SATURDAY, .move_tutor_outside
+	ifequal SUNDAY, .move_tutor_outside
 .move_tutor_inside
 	appear GOLDENRODGAMECORNER_MOVETUTOR
 	endcallback
